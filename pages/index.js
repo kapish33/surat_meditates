f531d55a5fde5c07eb5f8501e1b10672f00a8a59
Shiva <kapishsingh2209@gmail.com>
2023-02-05 23:31:16 +0530

    2n commit

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import Footer from '../src/components/Footer';
 import Header from '../src/components/Header';
+import Hero from '../src/components/Hero';
+import Pricing from '../src/components/Pricing';
+import Stats from '../src/components/Stats';
 
 const index = () => {
   return (
     <>
       <Header />
+      <Hero />
+      <Stats />
+      <Pricing />
       <Footer />
     </>
   );
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,33 +4,23 @@ import GurduevImage from '../../assets/guru-png.png';
 
 const Header = () => {
   return (
-    <header className='text-gray-600 body-font'>
+    <header className='text-gray-400 bg-gray-900 body-font'>
       <div className='container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center'>
-        <a className='flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0'>
-          {/* <svg
-            xmlns='http://www.w3.org/2000/svg'
-            fill='none'
-            stroke='currentColor'
-            stroke-linecap='round'
-            stroke-linejoin='round'
-            stroke-width='2'
-            className='w-10 h-10 text-white p-2 bg-indigo-500 rounded-full'
-            viewBox='0 0 24 24'>
-            <path d='M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5'></path>
-          </svg> */}
+        <a className='flex title-font font-medium items-center text-white mb-4 md:mb-0'>
           <Image
-            className='w-10 h-10 text-white p-2  rounded-full'
-            src={GurduevImage}npms
+            className='w-12 h-12 p-2  rounded-full'
+            src={GurduevImage}
+            alt='Mediatates '
           />
-          <span className='ml-3 text-xl'>Suart Meditates</span>
+          <span className='ml-3 text-xl'>Surta Mediatates</span>
         </a>
         <nav className='md:ml-auto flex flex-wrap items-center text-base justify-center'>
-          <a className='mr-5 hover:text-gray-900'>First Link</a>
-          <a className='mr-5 hover:text-gray-900'>Second Link</a>
-          <a className='mr-5 hover:text-gray-900'>Third Link</a>
-          <a className='mr-5 hover:text-gray-900'>Fourth Link</a>
+          <a className='mr-5 hover:text-white'>First Link</a>
+          <a className='mr-5 hover:text-white'>Second Link</a>
+          <a className='mr-5 hover:text-white'>Third Link</a>
+          <a className='mr-5 hover:text-white'>Fourth Link</a>
         </nav>
-        <button className='inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0'>
+        <button className='inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0'>
           Button
           <svg
             fill='none'
