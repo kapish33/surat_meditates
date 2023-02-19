834470085abcf40ef5c88d4fdd3de780728eda75
Shiva <kapishsingh2209@gmail.com>
2023-02-19 20:00:14 +0530

    a

---
+++ utils/nameConst.js
@@ -0,0 +1,6 @@
+export const social = {
+  ART_OF_LIVING_URL: 'https://www.artofliving.org',
+  FACEBOOK_URL: 'https://www.facebook.com/lavkeshcommunicates/',
+  INSTAGRAM_URL: 'https://www.instagram.com/lavkeshisherenow/',
+  TWITTER_URL: 'https://twitter.com/artofliving',
+};
